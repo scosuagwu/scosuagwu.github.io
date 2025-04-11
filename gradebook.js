e5519ec48c78f9063326864d0c47cf9b914aae5e
scosuagwu <sydneyosuagwu30@gmail.com>
2025-04-11 17:01:38 -0400

    add gradebook HTML and Javascript files

---
+++ gradebook.js
@@ -0,0 +1,12 @@
+function fetchGradeData() {
+  console.log("fetchGradeData called");
+  // This function will fetch grade data in the future
+}
+
+function populateGradebook() {
+  console.log("populateGradebook called");
+  // This function will populate the table in the future
+}
+
+fetchGradeData();
+populateGradebook();
